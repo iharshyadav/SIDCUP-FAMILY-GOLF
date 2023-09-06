17051a4dc8e98c677babc0026efb1cc5738ec1d4
Harsh Yadav <hy945196@gmail.com>
2023-09-06 23:05:52 +0530

    Day-4
    Created page-3 ,
    Used Javasript on cursor,and in page 3.

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ let crsr =document.querySelector(".cursor")
 
 let blur =document.querySelector(".cursor-blur") 
 
+let all = document.querySelectorAll(".nav h2")
+
+
+
+all.forEach(function(tr){
+    tr.addEventListener("mouseenter",function(){
+       crsr.style.scale = 4
+       crsr.style.border = "0.1px solid #fff"
+       crsr.style.backgroundColor = "transparent"
+       crsr.style.animation = " all ease 1s"
+    })
+
+    tr.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #95c11e"
+        crsr.style.backgroundColor = "#95c11e"
+    })
+})
+
 document.addEventListener("mousemove",function(e){
     crsr.style.left = e.x - 11 + "px"
     crsr.style.top = e.y - 11 + "px"
@@ -10,6 +29,32 @@ document.addEventListener("mousemove",function(e){
     blur.style.top = e.y - 140 + "px"
 })
 
+gsap.to(".quote1",{
+    top:"25%",
+    left:"14%",
+    scrollTrigger:{
+        trigger:".quote1",
+        scroller:"body",
+        // markers:true,
+        start:"top 95%",
+        end:"top 120%",
+        scrub:1
+    }
+})
+
+gsap.to(".quote2",{
+    bottom:"4%",
+    right:"18%",
+    scrollTrigger:{
+        trigger:".quote2",
+        scroller:"body",
+        // markers:true,
+        start:"top 140%",
+        end:"top 70%",
+        duration:0.5,
+        scrub:1
+    }
+})
 
 gsap.to(".nav",{
     backgroundColor:"black",
@@ -21,6 +66,7 @@ gsap.to(".nav",{
         // markers:true,
         start:"top -10%",
         end:"top -11%",
+        duration:0.5,
         scrub:1
     }
 })
